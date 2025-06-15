9945d72f11f0b0e2932bd99315aa98c78107d7fd
willyoubemyfriend <chiyaburro@gmail.com>
2025-06-15 16:40:29 -0400

    Update assets.js

--- js/assets.js
+++ js/assets.js
@@ -1,1 +1,33 @@
+const playerImg = new Image();
+playerImg.src = 'assets/player.png';
 
+const tileset = new Image();
+tileset.src = 'assets/tileset.png';
+
+const inventoryImg = new Image();
+inventoryImg.src = 'assets/inventory.png';
+
+const playerImage = new Image();
+playerImage.src = 'assets/player_image.png';
+
+const enemyIcons = new Image();
+enemyIcons.src = 'assets/enemy_icons.png';
+
+const enemyStatusesImg = new Image();
+enemyStatusesImg.src = 'assets/enemy_statuses.png';
+
+const creatureGrid = new Image();
+creatureGrid.src = 'assets/creature_grid.png';
+
+const assets = [playerImg, tileset, inventoryImg, playerImage, enemyIcons, enemyStatusesImg, creatureGrid];
+
+let assetsLoaded = 0;
+
+export function loadAssets(callback) {
+  assets.forEach(img => {
+    img.onload = () => {
+      loaded++;
+      if (loaded === assets.length) callback();
+    };
+  });
+}
