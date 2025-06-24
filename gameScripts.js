3251085fc49cefe3c5fd27372f200c419d083689
willyoubemyfriend <chiyaburro@gmail.com>
2025-06-24 16:48:29 -0400

    Update gameScripts.js

--- gameScripts.js
+++ gameScripts.js
@@ -1,9 +1,11 @@
+import { SUBEVENT_TYPES } from './eventSystem.js';
+
 export const GAME_EVENTS = {
     TEST_EVENT: {
         description: "Test dialogue event",
         subEvents: [
             {
-                type: "DIALOGUE",
+                type: SUBEVENT_TYPES.DIALOGUE, // Using the exported constant
                 lines: ["This is a test event!", "It works!"],
                 isBlocking: true,
                 isStarted: false,
