319c8a88ea9d4ae0d5396d556367c30dd7d83528
willyoubemyfriend <chiyaburro@gmail.com>
2025-06-25 12:56:17 -0400

    Update game.js

--- game.js
+++ game.js
@@ -149,7 +149,7 @@ function handleKeyPress(key) {
         if (key === ",") changeInventoryPage(inventory, -1);
     }
     
-    if (key === "t") triggerEvent("PARALLEL_DEMO"); 
+    if (key === "t") triggerEvent("MOVEMENT_TEST"); 
 }
 
 function canMoveInCurrentRoom(x, y) {
