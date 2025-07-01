d3d54106f93c5555f479ac70b5697f9cf06dd065
willyoubemyfriend <chiyaburro@gmail.com>
2025-07-01 13:01:27 -0400

    Update eventList.js

--- eventList.js
+++ eventList.js
@@ -1,23 +1,26 @@
 export const Events = {
-    complexScene: {
+    introScene: {
         steps: [
             [
+                // Dialogue starts
                 { 
                     type: "dialogue", 
-                    lines: ["Hey... are you seeing this?"] 
+                    lines: ["Welcome... to the pain chamber."] 
                 },
-                { 
+
+                // Group: Wait 1 second, then move player (while dialogue is still active)
+                {
                     type: "group",
                     steps: [
-                        [{ type: "wait", duration: 60 }],
-                        [{ type: "movePlayer", x: 5, y: 5 }]
+                        [ { type: "wait", duration: 60 } ],
+                        [ { type: "movePlayer", x: 5, y: 5 } ]
                     ]
                 }
             ],
             [
                 { 
                     type: "dialogue", 
-                    lines: ["Well... that just happened."] 
+                    lines: ["...Hope you're ready."] 
                 }
             ]
         ]
